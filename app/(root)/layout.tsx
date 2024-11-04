80f7e1d2c61502b04e9dd206bc78ca575655ef66
Octavio Lara Pinzón <octavio@MacBook-Pro-de-Octavio.local>
2024-11-03 22:35:11 -0600

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,23 @@
+import StreamVideoProvider from '@/providers/StreamClientProvider'
+import { Metadata } from 'next';
+import React, { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: "Walrus",
+  description: "Your best coworker",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
+
+const rootLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <main>
+      <StreamVideoProvider>
+        {children}
+      </StreamVideoProvider>
+    </main>
+  )
+}
+
+export default rootLayout
